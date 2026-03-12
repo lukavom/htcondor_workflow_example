4d4cac7971c0425ccfb6f227ba0b1c77c00200ba
Luka Vomberg <lukavomberg@gmail.com>
2026-03-12 09:39:33 +0100

    add atlas example with xrootd

--- examples/atlas/condor-jobscript.sh
+++ examples/atlas/condor-jobscript.sh
@@ -2,8 +2,10 @@
 # properties = {properties}
 export HOME=$(pwd)
 . /etc/profile
-tar --extract -z --file venv_snakemake.tar.gz
+ls -l
+tar --extract -z --file /cephfs/user/s6luvomb/venvs/venv_snakemake.tar.gz
 module load miniforge/24.11.2-1-py312
 . venv_snakemake/bin/activate
 
-{exec_job}
+
+snakemake "$@"
